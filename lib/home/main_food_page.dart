374bd7012fb5edefa46231fa28cafd31f4fae8a1
honsmart <adegboyeopeyemi580@gmail.com>
2022-12-20 12:56:37 +0100

    main food page

---
+++ lib/home/main_food_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery/utils/colors.dart';
+import 'package:food_delivery/widgets/big_text.dart';
+
+class MainFoodPage extends StatefulWidget {
+  const MainFoodPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainFoodPage> createState() => _MainFoodPageState();
+}
+
+class _MainFoodPageState extends State<MainFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [ Container(
+            child: Container(
+              margin: const  EdgeInsets.only(top: 45, bottom: 15),
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween ,
+                  children: [
+                    Column(
+                      children:  [
+                        BigText(text: "Nigeria", color: AppColors.mainColor, size: 20 ,),
+                        Text("City"),
+                      ],
+                    ),
+                    Center(
+                      child: Container(
+                        width: 45,
+                        height: 45,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: AppColors.mainColor,
+                        ),
+                        child: const  Icon(Icons.search, color: Colors.white),
+                      ),
+                    )
+                  ],
+                )
+            )
+        ),],
+      )
+    );
+  }
+}
